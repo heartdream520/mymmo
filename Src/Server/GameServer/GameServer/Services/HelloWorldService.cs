3ea13ac4a9dec6329a9ec8969368570202920c28
浩禹 曲 <1258005369@qq.com>
2022-12-04 12:24:29 +0800

    12.4-资源整理，UI资源导入

--- Src/Server/GameServer/GameServer/Services/HelloWorldService.cs
+++ Src/Server/GameServer/GameServer/Services/HelloWorldService.cs
@@ -17,8 +17,11 @@ namespace GameServer.Services
         }
         public void Start()
         {
+
+            //使用方法  OnFirstTestRequest  处理 消息 FirstTestRequest
             MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<FirstTestRequest>(this.OnFirstTestRequest);
         }
+        //处理消息FirstTestRequest的方法
         public void OnFirstTestRequest(NetConnection<NetSession>sender,FirstTestRequest request)
         {
             Log.InfoFormat("OnFirstTestRequest: HelloWorld:{0}", request.Helloword);
