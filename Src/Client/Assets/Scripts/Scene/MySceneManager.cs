0fc3309501e878fd9925bc7096811e13106d7eb7
浩禹 曲 <1258005369@qq.com>
2022-12-11 16:17:23 +0800

    12-11-天空盒

---
+++ Src/Client/Assets/Scripts/Scene/MySceneManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MySceneManager : MonoSingleton<MySceneManager>
+{
+    UnityAction<float> onProgress = null;
+
+    // Use this for initialization
+    protected override void OnStart()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void LoadScene(string name)
+    {
+        GameDataManager.SetString("lodeing_scene",name);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Load_Scenes");
+        //StartCoroutine(LoadLevel(name));
+    }
+
+    IEnumerator LoadLevel(string name)
+    {
+        Debug.LogFormat("LoadLevel: {0}", name);
+        AsyncOperation async = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(name);
+        async.allowSceneActivation = true;
+        async.completed += LevelLoadCompleted;
+        while (!async.isDone)
+        {
+            if (onProgress != null)
+                onProgress(async.progress);
+            yield return null;
+        }
+    }
+
+    private void LevelLoadCompleted(AsyncOperation obj)
+    {
+        if (onProgress != null)
+            onProgress(1f);
+        Debug.Log("LevelLoadCompleted:" + obj.progress);
+    }
+}
